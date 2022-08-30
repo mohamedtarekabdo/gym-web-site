31fe2c7901b3d7edd764e0f36d5438cce6c2bf92
mohamedtarek <mohamedtarekabdo0@gmail.com>
2022-08-30 02:37:43 +0200

    SCROLL

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,33 @@ const scrollHeader = () =>{
 window.addEventListener("scroll",scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]")
 
+const scrollActive = () =>{
+   const scrollY = window.pageXOffset
 
+   sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop -58,
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(".nav__menu a[href*=' + sectionId + ']" )
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+               sectionsClass.classList.add("active-link")
+            } else{
+               sectionsClass.classList.remove("active-link")
+            }
+   })
+}
+window.addEventListener("scroll",scrollActive)
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollup = () =>{
+   const scrollup = document.getElementById("scroll-up")
+   //when the scroll is higher than 350 viewport height , add the show scroll class to a tag with the scrollup
+   this.scrollY >= 350 ? scrollup.classList.add("show-scroll")
+                      : scrollup.classList.remove("show-scroll")
+}
+window.addEventListener("scroll", scrollup)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
